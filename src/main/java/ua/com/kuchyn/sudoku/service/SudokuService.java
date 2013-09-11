c14b30bcb6e16c5d48568b8768c4b970b1de1336
viktor <kuchin.victor@gmail.com>
2013-09-11 10:12:26 +0300

    Extracted interface for service

---
+++ src/main/java/ua/com/kuchyn/sudoku/service/SudokuService.java
@@ -0,0 +1,15 @@
+package ua.com.kuchyn.sudoku.service;
+
+import ua.com.kuchyn.sudoku.model.Sudoku;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: viktor
+ * Date: 9/11/13
+ * Time: 10:03 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface SudokuService
+{
+    Sudoku generateSudoku();
+}
