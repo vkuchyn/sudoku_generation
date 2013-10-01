a32942668f733e470025b2875ba6afd3f7ade953
viktor <kuchin.victor@gmail.com>
2013-10-01 21:19:46 +0300

    Shuffle rows test

--- src/test/java/ua/com/kuchyn/sudoku/service/SudokuServiceTest.java
+++ src/test/java/ua/com/kuchyn/sudoku/service/SudokuServiceTest.java
@@ -4,7 +4,10 @@ import org.junit.Test;
 import ua.com.kuchyn.sudoku.algorythm.ua.com.kuchyn.model.Sudoku;
 import ua.com.kuchyn.sudoku.service.shuffle.ShuffleSudokuService;
 
+import java.util.Arrays;
+
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -28,6 +31,8 @@ public class SudokuServiceTest
             {6, 7, 8, 9, 1, 2, 3, 4, 5},
             {9, 1, 2, 3, 4, 5, 6, 7, 8}};
 
+    private final SudokuService sudokuService = new ShuffleSudokuService();
+
     /**
      * this test should generate initial state
      */
@@ -35,7 +40,6 @@ public class SudokuServiceTest
     public void shouldGenerateSudokuInitialState() throws Exception
     {
         //Given
-        SudokuService sudokuService = new ShuffleSudokuService();
 
         //When
         Sudoku actualSudoku = sudokuService.generateInitialState();
@@ -44,4 +48,36 @@ public class SudokuServiceTest
         assertThat(actualSudoku.getSudokuField(), is(SUDOKU_INITIAL_STATE));
 //        git pull origin xpinjection
     }
+
+    @Test
+    public void shouldShuffleRows() throws Exception
+    {
+        //Given
+        Sudoku initialSudoku = sudokuService.generateInitialState();
+
+        //When
+        Sudoku actualSudoku = sudokuService.shuffleRows(initialSudoku);
+
+        //Then
+        assertThat(actualSudoku.getSudokuField(), is(not(SUDOKU_INITIAL_STATE)));
+
+        for (int i = 0; i < 9; i++)
+        {
+            assertThat(assertRowIsPresent(actualSudoku, i), is(true));
+        }
+    }
+
+    private boolean assertRowIsPresent(Sudoku actualSudoku, int row)
+    {
+        int cellSection = row / 3;
+        return assertRowsEquals(actualSudoku, cellSection, row) ||
+                assertRowsEquals(actualSudoku, cellSection + 1, row) ||
+                assertRowsEquals(actualSudoku, cellSection + 2, row);
+    }
+
+    private boolean assertRowsEquals(Sudoku actualSudoku, int cellSection, int row)
+    {
+        return Arrays.equals(actualSudoku.getSudokuField()[cellSection], SUDOKU_INITIAL_STATE[row]);
+    }
+
 }
